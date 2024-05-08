633331bdf196de0bfd80e3d581a3fbb6184293ee
catOWO2011 <catOWO2011@gmail.com>
2024-05-08 01:57:57 -0400

    Added firebase

--- src/utils/firebase.utils.js
+++ src/utils/firebase.utils.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { collection, getDoc, getFirestore } from "firebase/firestore";
+import { collection, doc, getDocs, getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
@@ -8,18 +8,27 @@ const firebaseConfig = {
   projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_FIREBASE_APP_ID
+  appId: import.meta.env.VITE_APP_FIREBASE_APP_ID,
 };
 
-const firebaseApp = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
 const db = getFirestore(app);
 
-// const querySnapshot = await getDocs(collection(db, "cities"));
-// querySnapshot.forEach((doc) => {
-//   // doc.data() is never undefined for query doc snapshots
-//   console.log(doc.id, " => ", doc.data());
-// });
-const querySnapshot = await getDoc(collection(db, 'quizzes'));
+const getAllDocuments = async (docName) => {
+  const docs = [];
 
+  const querySnapshot = await getDocs(collection(db, docName));
+  querySnapshot.forEach((doc) => {
+    docs.push({
+      id: doc.id,
+      ...doc.data()
+    });
+  });
 
+  return docs;
+};
+
+export const getQuizzes = async () => {
+  return await getAllDocuments('quizzes');
+};
