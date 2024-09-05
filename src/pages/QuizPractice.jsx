a54c5bb0871b906601b0f0e3e89bf2f83f1fe0dc
catOWO2011 <catOWO2011@gmail.com>
2024-09-05 14:52:18 -0400

    Improve evaluation of answers

--- src/pages/QuizPractice.jsx
+++ src/pages/QuizPractice.jsx
@@ -8,6 +8,7 @@ import FillInBlankField from "../components/FillInBlankField";
 import { questionConstantsText } from "../constants/question";
 import MultipleChoiceField from "../components/MultipleChoiceField";
 import { RiCheckboxCircleLine, RiCloseCircleLine } from "react-icons/ri";
+import MultipleResponseField from "../components/MultipleResponseField";
 
 const StyledButton = styled(Button)`
   &:hover {
@@ -30,22 +31,27 @@ export default function QuizPractice() {
   const getQuestionField = (type, question) => {
     const fields = {
       [`${questionConstantsText.MULTIPLE_CHOICE}`]: (<MultipleChoiceField question={question}/>),
-      [`${questionConstantsText.FILL_IN_THE_BLANKS}`]: (<FillInBlankField question={question}/>)
+      [`${questionConstantsText.FILL_IN_THE_BLANKS}`]: (<FillInBlankField question={question}/>),
+      [`${questionConstantsText.MULTIPLE_RESPONSE}`]: (<MultipleResponseField question={question}/>)
     };
 
     return fields[type];
   };
 
   const evaluate = (correctOptions, answers) => {
-    if (correctOptions.length === Object.keys(answers).length) {
-      for (const { id, optionText } of correctOptions) {
-        if (answers[id]) {
-          if (answers[id].toLowerCase().trim() !== optionText.toLowerCase().trim()) {
+    if (answers) {
+      if (correctOptions.length === Object.keys(answers).length) {
+        for (const { id, optionText } of correctOptions) {
+          if (answers[id]) {
+            if (answers[id].toLowerCase().trim() !== optionText.toLowerCase().trim()) {
+              return false;
+            }
+          } else {
             return false;
           }
-        } else {
-          return false;
         }
+      } else {
+        return false;
       }
     } else {
       return false;
@@ -70,6 +76,15 @@ export default function QuizPractice() {
       <Flex gap="small" vertical className="bg-[#e5fbd6] p-4 mb-4 rounded-md">
         <Progress percent={currentPercent} size={[,20]} showInfo={false}/>
       </Flex>
+      {
+        currentIndexQuestion < totalQuestions &&
+        questions[currentIndexQuestion].text &&
+        <Flex className="p-4 bg-[#FBF0B2] rounded-md">
+          <span className="text-xl">
+            { questions[currentIndexQuestion].text}
+          </span>
+        </Flex>
+      }
       <Form form={form} className="h-full flex-col justify-between flex p-1" onFinish={onFinish}>
         {
           currentIndexQuestion < totalQuestions &&
@@ -128,7 +143,7 @@ export default function QuizPractice() {
               color="#7eabb1"
             />
           </div>
-          <div className="text-3xl flex flex-row items-center">
+          <div className="text-2xl flex flex-row items-center">
             <span className="text-[#7eabb1]">
               Correct Answer
             </span>
@@ -144,7 +159,7 @@ export default function QuizPractice() {
               color="#FF8080"
             />
           </div>
-          <div className="text-3xl flex flex-row items-center">
+          <div className="text-2xl flex flex-row items-center">
             <span className="text-[#FF8080]">
               The correct answer is: {JSON.parse(questions[currentIndexQuestion].options).filter(({ isCorrect }) => isCorrect).map(({optionText }) => optionText).join(',')}
             </span>
